adb4c10b0ad44f86273278bc43df16c5098c8208
annmaryvinod <annmarypriyavinod@gmail.com>
2024-06-22 18:29:58 +0530

    signup

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -30,7 +30,7 @@ export const userSignUp = async (req:Request,res: Response,next:NextFunction) =>
     const hashedPassword = await hash(password,10);
     const user = new User({name,email,password: hashedPassword});
     await user.save();
-    return res.status(200).json({message: "OK",id : user._id.toString()});
+    return res.status(201).json({message: "OK",id : user._id.toString()});
 
 
     }catch(error){
